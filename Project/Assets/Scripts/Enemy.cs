a121cdeac2b67e82529224cb464e83e8052d1ead
Dopost <dopostt@gmail.com>
2023-01-09 18:11:26 +1000

    enemy

--- Project/Assets/Scripts/Enemy.cs
+++ Project/Assets/Scripts/Enemy.cs
@@ -1,23 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Enemy : MonoBehaviour
+public class Enemy: MonoBehaviour
 {
-    private float speed = 3.5f;
-    private Vector3 dir;
-    private SpriteRenderer sprite;
+    public float speed;
+    public Vector3[] position;
 
-    private void Start()
+    private int currentTarget;
+
+    public void FixedUpdate()
     {
-        dir = transform.right;
+        transform.position = Vector3.MoveTowards(transform.position, position[currentTarget], speed);
+
+        if(transform.position == position[currentTarget])
+        {
+            if(currentTarget < position.Length - 1)
+            {
+                currentTarget++;
+            }else
+            {
+                currentTarget = 0;
+            }
+        }
     }
 
-    private void Move()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.1f + transform.right * dir.x * 0.7f, 0.1f);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
 
-        if (colliders.Length > 0) dir *= -1f;
-        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, Time.deltaTime);
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
